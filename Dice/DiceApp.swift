0ba4698db18e7f8ff3884049903c2eac158f7aa1
kotathefriends <5070kouta@gmail.com>
2023-10-14 17:18:04 +0900

    Initial Commit

---
+++ Dice/DiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceApp.swift
+//  Dice
+//
+//  Created by KOTA TAKAHASHI on 2023/10/14.
+//
+
+import SwiftUI
+
+@main
+struct DiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
